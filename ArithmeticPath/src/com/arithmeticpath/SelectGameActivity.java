9836185a3fa81b5aad37f0b65ef078dbea225131
sinegordon <sinegordon@gmail.com>
2012-11-19 17:42:30 +0300

    0.0.5

--- ArithmeticPath/src/com/arithmeticpath/SelectGameActivity.java
+++ ArithmeticPath/src/com/arithmeticpath/SelectGameActivity.java
@@ -25,6 +25,10 @@ public class SelectGameActivity extends Activity implements OnClickListener {
 	    	intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 	    	startActivity(intent);
 			break;
+		case R.id.SchoolButton:
+			break;
+		case R.id.btn2:
+			finish();
 		default:
 			break;
 		}
@@ -34,6 +38,9 @@ public class SelectGameActivity extends Activity implements OnClickListener {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_game);
+        findViewById(R.id.UniversityButton).setOnClickListener(this);
+        findViewById(R.id.SchoolButton).setOnClickListener(this);
+        findViewById(R.id.btn2).setOnClickListener(this);
     }
 
 }
--- ArithmeticPath/src/com/arithmeticpath/UniversityActivity.java
+++ ArithmeticPath/src/com/arithmeticpath/UniversityActivity.java
@@ -1,13 +1,19 @@
 package com.arithmeticpath;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 
-public class UniversityActivity extends Activity implements OnClickListener {
+public class UniversityActivity extends Activity implements OnClickListener, OnTouchListener {
 
 	private static Game game = null;
+	
+	private SurfaceView gameview = null;
 	
 	public static void setGame(Game game) {
 		UniversityActivity.game = game;
@@ -15,14 +21,26 @@ public class UniversityActivity extends Activity implements OnClickListener {
 	
 	@Override
 	public void onClick(View view) {
-		
+		switch (view.getId()) {
+		case R.id.btn2:
+			finish();
+		default:
+			break;
+		}
 	}
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.select_game);
+        setContentView(R.layout.university);
+        gameview = (SurfaceView)findViewById(R.id.surfaceView1);
+        findViewById(R.id.btn2).setOnClickListener(this);
     }
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		
+		return false;
+	}
 
 }
